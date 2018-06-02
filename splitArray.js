be3f3b77f837d90089fae03e55b284deeba30458
TinoRoman <romanm.tino@gmail.com>
2018-06-02 12:09:19 +0300

    Update splitArray.js
    Added faster non functional version

--- splitArray.js
+++ splitArray.js
@@ -1,9 +1,24 @@
-const splitArray = (arr, n) => 
+const getLast = (arr) => arr[arr.length - 1]
+
+const splitArrayF = (n, arr) => 
   arr.reduce( (acc, num) => {
-    const last = acc[acc.length - 1];
+    const last = getLast(acc)
     return last && last.length < n
       ? [...acc.splice(0, acc.length - 1), [...last, num ]]
       : [...acc, [num]]
+  },
+  []
+)
+
+const splitArray = (arr, n) => 
+  arr.reduce( (acc, num) => {
+    const last = getLast(acc)
+    if (last && last.length < n) {
+      getLast(acc).push(num)
+    } else {
+      acc.push([num])
+    }
+    return acc
   },
   []
 )
