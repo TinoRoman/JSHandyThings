0cefb0146837af464800faa691c8d6e3ba73f51b
TinoRoman <romanm.tino@gmail.com>
2018-07-03 01:00:45 +0300

    Create maybe-example.js
    Example of using maybe monad with ramda

---
+++ maybe-example.js
@@ -0,0 +1,39 @@
+const R = require('ramda')
+const Maybe = require('maybe')
+
+const toMaybeUser = ( { age, name, sex } ) => ( { 
+  age  : Maybe( age ),
+  name : Maybe( name ),
+  sex  : Maybe( sex ),
+} )
+
+const isAMan = ( user ) => user.sex.isJust()
+  ? user.sex.value() === 'M'
+  : false
+
+const hasAge = ( user ) =>  user.age.isJust()
+
+const manWithName = R.both( isAMan, hasAge )
+
+const getName = ( user ) => user.name.isJust()
+  ? user.name.value()
+  : 'No name'
+
+const getOldest = ( userA, userB ) => userA.age > userB.age
+  ? userA
+  : userB
+
+const getOldestMan = ( users ) => users
+  .map( toMaybeUser )
+  .filter( R.both( manWithName ) )
+  .reduce( getOldest )
+
+const users = [
+  { age: 17,   name: 'Tom',  sex: 'M' },
+  { age: null, name: 'Kate', sex: 'F' },
+  { age: 37,   name: null,   sex: 'M' }
+]
+
+const oldestMan = R.pipe( getOldestMan, getName )( users )
+
+console.log( oldestMan )
